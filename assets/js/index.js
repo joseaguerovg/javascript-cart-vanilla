5c30783ad054483d421300c2d85f5bd7fa50b2c7
José Aguero <jaguero@moldeable.com>
2020-10-03 16:38:20 -0300

    Evento y efecto para mostrar/ocultar carro

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,26 @@
 const d = document,
-    w = window;
+    w = window,
+    shoppingCart = d.querySelector('.shopping-cart');
 
 const addProductToCart = () => {
-    console.log('a');
+    shoppingCart.classList.add('open-cart');
 }
+
+const closeCart = () => {
+    shoppingCart.classList.remove('open-cart');
+}
+
+d.addEventListener("click", (e) => {
+    if (e.target.matches(".add-to-cart")) {
+        addProductToCart(e);
+    }
+
+    if (shoppingCart.classList.contains('open-cart') && !e.target.matches('.add-to-cart')) {
+        closeCart();
+    }
+
+})
+
+shoppingCart.addEventListener('click', (e) => {
+    e.stopPropagation();
+});
